65b9e7b8b6a6df5fe50944dc9f255310a22c5782
Warren <warrcamp@gmail.com>
2024-12-30 13:49:17 -0700

    Added movement key functionality for A and D

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         
         #limit FPS to 60
