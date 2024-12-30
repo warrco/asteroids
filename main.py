5af14fc05542af729365f0a1d23208ab75399e99
Warren <warrcamp@gmail.com>
2024-12-30 13:31:06 -0700

    Added player sprite to the middle of the screen

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE
+from constants import *
+from player import Player
 
 def main():
     #Initialize pygame and set the screen
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Set frame-rate
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #Set up player object
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     #draw the screen
     while True:
@@ -12,7 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        
+        #limit FPS to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
